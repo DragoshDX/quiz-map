151f59e0f2d363f77f05824001d5aa7a0ec16af3
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-12-02 16:52:07 +0200

    quzi button layout

--- src/components/core/Quiz.jsx
+++ src/components/core/Quiz.jsx
@@ -4,16 +4,16 @@ import React from 'react';
 
 export const Quiz = () => {
   return (
-    <div className="lg:w-1/2 mx-auto">
+    <div className="px-6 lg:px-0 lg:w-1/2 mx-auto">
       <div className="flex gap-8 mb-5">
-        <div>timer</div>
+        <div className="hidden lg:block">timer</div>
 
         <p className="bg-gray-50 shadow-lg text-center flex-grow p-2 rounded-lg">
           Selecteaza judetul corespunzator:
         </p>
       </div>
 
-      <ul className="grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5">
+      <ul className="grid lg:grid-cols-2 gap-y-3 lg:gap-x-8 lg:gap-y-5 px-6 lg:px-0">
         {shuffle(units)
           .slice(0, 4)
           .map(({ id, name }) => {
